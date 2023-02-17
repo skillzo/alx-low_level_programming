6e9a8b546abc8d4f25671e395886f139eaec49a2
skillzo <skillzfire@yahoo.com>
2023-02-16 16:20:21 -0800

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <studio.h>
+
+/**
+ * main - prints "Programming is like building a multilingual puzzle,
+ * puzzle, followed by newline."
+ * return 0.
+ */
+
+init main(void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return(0);
+}
