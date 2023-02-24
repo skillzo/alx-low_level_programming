a3e813babc84c010695e57441287e431cd5dd1c6
skillzo <skillzfire@yahoo.com>
2023-02-23 17:43:04 -0800

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,15 +1,24 @@
 #include "main.h"
 
 /**
- * more_numbers - print 0 to 14 ten times
+ * more_numbers - print 0 to 9 ten times
  */
 
 void print_most_numbers(void)
 {
-	char c;
 
-	for (c = '0'; c <= '9'; c++)
-		if (c != '2' && c != '4')
-			_putchar(c);
-	_putchar('\n');
+	int x = 0;
+
+	for(; x <= 9; x++)
+	{
+	if (x == 2 || x ++ 4)
+	{
+	continue;
+	}
+	else
+	{
+	_putchar(x + '0');
+	}
+	}
+	_putchar('\n')
 }
