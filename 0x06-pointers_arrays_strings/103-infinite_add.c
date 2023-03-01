3432de3834ca9b5eeec24b98bd25533381d81763
skillzo <skillzfire@yahoo.com>
2023-03-01 14:47:46 -0800

    task 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,9 +1,27 @@
 #include "main.h"
 
-
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+/**
+ * rot13 - encodes a string in rot13
+ * @s: string to be encoded
+ * Return: the resulting strring
+ */
+char *rot13(char *s)
 {
-	int count;
+	int i, j;
 
-	for (count = 0; count <= )
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+				break;
+			}
+	    }
+	}
+	return (s);
 }
